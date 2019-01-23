9b91a9d0985bbc3b72c6f893470625b1484cfa37
Mussarath Sheriff <mussarathsheriff@Mussaraths-MacBook-Air.local>
2019-01-23 14:27:30 +0530

    Updates

---
+++ precache-manifest.7d80154e3cbea10e46278433542f4ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "013eac1cb96e93e7fa73",
+    "url": "/mushhrath/sheriff-profile/static/js/runtime~main.013eac1c.js"
+  },
+  {
+    "revision": "aa58ea8ee67aff771f0f",
+    "url": "/mushhrath/sheriff-profile/static/js/main.aa58ea8e.chunk.js"
+  },
+  {
+    "revision": "202cef419bd44d56bafe",
+    "url": "/mushhrath/sheriff-profile/static/js/1.202cef41.chunk.js"
+  },
+  {
+    "revision": "aa58ea8ee67aff771f0f",
+    "url": "/mushhrath/sheriff-profile/static/css/main.62bb9e8f.chunk.css"
+  },
+  {
+    "revision": "202cef419bd44d56bafe",
+    "url": "/mushhrath/sheriff-profile/static/css/1.dec6f4e3.chunk.css"
+  },
+  {
+    "revision": "5dd807bdf7f74ad50aee479bc91ed74b",
+    "url": "/mushhrath/sheriff-profile/index.html"
+  }
+];
